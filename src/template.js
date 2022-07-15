f734b78f60a4170e411ed64e29946b0304a42974
meganbeek98 <meganbeek98@gmail.com>
2022-07-15 04:42:09 -0400

    test worked, gonna delete and run again

--- src/template.js
+++ src/template.js
@@ -192,7 +192,7 @@ const generateProfileHtml = (teamMemProfile) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moral Booster</title>
+    <title>Profile Generator</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
