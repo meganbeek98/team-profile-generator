c23d779ee80765d9efe0d4a70a365d8343b7737f
meganbeek98 <meganbeek98@gmail.com>
2022-07-14 23:27:05 -0400

    setup for writing, copying, exporting files

---
+++ src/generate-site.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// write files
+const writeFile = fileContent => {
+
+};
+
+// copy files
+const copyFile = () => {
+
+};
+
+
+// export files
+module.exports = { writeFile, copyFile };
