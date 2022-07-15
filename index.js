a32a17c5d04420a4cd1d9d5828f339d94a96a2a4
meganbeek98 <meganbeek98@gmail.com>
2022-07-14 23:46:27 -0400

    writeFile & copyFile additions

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 
--- src/generate-site.js
+++ src/generate-site.js
@@ -1,15 +1,38 @@
 const fs = require('fs');
 
-// write files
+// write file
 const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
+      resolve({
+        ok: true,
+        message: 'HTML file created!'
+      });
+    });
+  });
 };
 
-// copy files
+// copy file
 const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
+      resolve({
+        ok: true,
+        message: 'Stylesheet file created!'
+      });
+    });
+  });
 };
 
 
-// export files
-module.exports = { writeFile, copyFile };
+module.exports = { writeFile, copyFile }; 
