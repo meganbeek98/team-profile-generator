5cb278d32955616135978e4d0380f10c294964a2
meganbeek98 <meganbeek98@gmail.com>
2022-07-15 00:02:17 -0400

    Connections to files from index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// required variables
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Connections to files
+const employee = require('./lib/employee.js');
+const manager = require('./lib/manager.js');
+const engineer = require('./lib/engineer.js');
+const intern = require('./lib/intern.js');
+
+
 
 // prompts for input of manager information
 
